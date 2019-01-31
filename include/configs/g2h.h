197690619bb9ee9befee821c41e43a537843dfd6
Steve Sakoman <steve@sakoman.com>
2019-01-31 12:37:14 -1000

    g2h.h: remove previous rescue boot and net boot code

--- include/configs/g2h.h
+++ include/configs/g2h.h
@@ -83,40 +83,20 @@
 #define CONFIG_DT ""
 #endif
 
-#ifdef CONFIG_BOOT_FROM_SPI
-#define CONFIG_BOOT_DEV "nandboot"
-#define CONFIG_RESCUE_BOOT "rescue_nor_boot"
-#else
 #define CONFIG_BOOT_DEV "mmcboot"
-#define CONFIG_RESCUE_BOOT "rescue_sd_boot"
-#endif
 
 #define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"image=boot/zImage\0" \
-	"splash=/boot/splash.bmp\0" \
+	"splash=boot/splash.bmp\0" \
 	"splashpos=m,m\0" \
 	"fdt_addr=0x18000000\0" \
 	"boot_fdt=try\0" \
-	"ip_dyn=yes\0" \
 	"console=" CONFIG_CONSOLE_DEV "\0" \
 	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0" \
 	"mmcpart=1\0" \
 	"mmcroot=" CONFIG_MMCROOT " rw rootfstype=ext4\0" \
-    "fdt_image=boot/" CONFIG_DT "\0" \
-	"update_sd_firmware=" \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"if mmc dev ${mmcdev}; then "	\
-			"if ${get_cmd} ${update_sd_firmware_filename}; then " \
-				"setexpr fw_sz ${filesize} / 0x200; " \
-				"setexpr fw_sz ${fw_sz} + 1; "	\
-				"mmc write ${loadaddr} 0x2 ${fw_sz}; " \
-			"fi; "	\
-		"fi\0" \
+	"fdt_image=boot/" CONFIG_DT "\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} quiet " \
 		"root=${mmcroot} consoleblank=0 vt.global_cursor_default=0\0" \
 	"loadimage=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
@@ -124,55 +104,12 @@
 	"mmcboot=echo Booting from mmc ...; mmc dev ${mmcdev}; mmc rescan; " \
         "run loadimage; run loadfdt; run mmcargs; " \
             "bootz ${loadaddr} - ${fdt_addr}; \0" \
-	"netargs=setenv bootargs console=${console},${baudrate} quiet " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"run netargs; " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${image}; " \
-		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
-			"if ${get_cmd} ${fdt_addr} ${fdtfile}; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
-			"else " \
-				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi; " \
-		"else " \
-			"bootz; " \
-		"fi;\0" \
-    "check_update=if test ${swupdate} = true; then run " CONFIG_RESCUE_BOOT "; " \
-        "else run " CONFIG_BOOT_DEV "; fi; \0" \
-    "nandargs=setenv bootargs console=${console},${baudrate} quiet " \
-        "${nandroot} consoleblank=0 vt.global_cursor_default=0 \0" \
-    "nandroot=ubi.mtd=2,2048 root=ubi0:rootfs0 rootfstype=ubifs \0" \
-    "nandboot=echo Booting from nand ...; " \
-        "run nandargs; " \
-        "nand read ${fdt_addr} 0x0000000 0x0080000; " \
-        "nand read ${loadaddr} 0x0080000 0x0A00000; " \
-        "bootz ${loadaddr} - ${fdt_addr}; \0" \
-	"swupdate=false \0" \
-	"rescue_addr=0x19000000 \0" \
-    "rescue_image=rescue-image.ext3.gz \0" \
-	"load_sd_rescue_image=fatload mmc ${mmcdev}:${mmcpart} ${rescue_addr} ${rescue_image}\0" \
-    "rescue_root=/dev/ram0 rw initrd=0x19000000,5M \0" \
-    "rescue_args=setenv bootargs console=${console},${baudrate} quiet rdinit=/sbin/init " \
-        "root=${rescue_root} consoleblank=0 vt.global_cursor_default=0 \0" \
-    "rescue_sd_boot=echo Rescue boot ...; run rescue_args; run load_sd_rescue_image; " \
-        "run loadimage; run loadfdt; bootz ${loadaddr} - ${fdt_addr}; \0" \
 
 #define CONFIG_BOOTCOMMAND \
     "setenv panel ${board_rev}; " \
     "load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${splash}; " \
     "bmp display ${loadaddr}; " \
-    "run check_update; "
+    "run mmcboot; "
 
 #define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
 
